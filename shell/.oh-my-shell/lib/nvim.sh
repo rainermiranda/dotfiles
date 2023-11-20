adb8858623e6a499d0c12ee7726291a5c2dc48d4
Rainer <rainer@sti.ufpb.br>
2023-11-20 14:06:08 -0300

    Added some functions to nvim environment

--- shell/.oh-my-shell/lib/nvim.sh
+++ shell/.oh-my-shell/lib/nvim.sh
@@ -1,1 +1,15 @@
-echo "lloadddeeeedda"
+function is_nvim_present() {
+    command -v nvim > /dev/null
+    if [ ${?} -ne 0 ]; then
+        echo "ERROR: nvim is not installed"
+        return 1
+    fi
+}
+
+function get_nvim_version() {
+    is_nvim_present
+    if [ ${?} -eq 0 ]; then
+        nvim_version=$(nvim -v |head -1|awk -F"v" '{print $2}')
+        echo ${nvim_version}
+    fi 
+}
