ab34049e01ea2e3ea68ddd72db71ecdcfbfaf405
Rainer <rainer@sti.ufpb.br>
2023-11-21 09:10:13 -0300

    verify if nvim is up to date

--- shell/.oh-my-shell/lib/nvim.sh
+++ shell/.oh-my-shell/lib/nvim.sh
@@ -1,15 +1,24 @@
 function is_nvim_present() {
     command -v nvim > /dev/null
     if [ ${?} -ne 0 ]; then
-        echo "ERROR: nvim is not installed"
         return 1
     fi
 }
 
-function get_nvim_version() {
+function is_nvim_latest_version() {
     is_nvim_present
-    if [ ${?} -eq 0 ]; then
-        nvim_version=$(nvim -v |head -1|awk -F"v" '{print $2}')
-        echo ${nvim_version}
-    fi 
+    if [ ${?} -ne 0 ]; then
+        echo "ERROR: Unable to locate nvim"
+        return 1
+    fi
+    c_version=$(nvim -v |head -1|awk -F" " '{print $2}')
+    l_version=`git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/neovim/neovim.git '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3`
+    if [ ${c_version} != ${l_version} ]; then
+        echo "The local NVIM version (${c_version}) is different from the latest version (${l_version})"
+    else
+        echo "NVIM is up to date (${c_version} = ${l_version})"
+    fi
 }
+
+is_nvim_latest_version
+
