7d7e78c59785268451552196afec3c91ee88de84
Rainer <rainer@sti.ufpb.br>
2023-11-23 10:08:47 -0300

    functio nvim_check_version update

--- shell/.oh-my-shell/lib/nvim.sh
+++ shell/.oh-my-shell/lib/nvim.sh
@@ -1,24 +1,15 @@
-function is_nvim_present() {
+function nvim_check_version() {
     command -v nvim > /dev/null
     if [ ${?} -ne 0 ]; then
-        return 1
-    fi
-}
-
-function is_nvim_latest_version() {
-    is_nvim_present
-    if [ ${?} -ne 0 ]; then
         echo "ERROR: Unable to locate nvim"
-        return 1
-    fi
-    c_version=$(nvim -v |head -1|awk -F" " '{print $2}')
-    l_version=`git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/neovim/neovim.git '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3`
-    if [ ${c_version} != ${l_version} ]; then
-        echo "The local NVIM version (${c_version}) is different from the latest version (${l_version})"
     else
-        echo "NVIM is up to date (${c_version} = ${l_version})"
+        c_version=$(nvim -v |head -1|awk -F" " '{print $2}')
+        l_version=`git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags https://github.com/neovim/neovim.git '*.*.*' | tail --lines=1 | cut --delimiter='/' --fields=3`
+        if [ ${c_version} != ${l_version} ]; then
+            echo "The local NVIM version (${c_version}) is different from the latest version (${l_version})"
+        else
+            echo "NVIM is up to date (${c_version} = ${l_version})"
+        fi
     fi
 }
-
-is_nvim_latest_version
 
